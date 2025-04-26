a381d1c25002b7ccdf471896b16c7977689a83b9
Hadeel-Jumaa <hadeelshjumaa.119@gmail.com>
2025-04-26 07:51:15 +0300

    new update connect database

--- DB/connection.js
+++ DB/connection.js
@@ -1,25 +1,12 @@
 import mongoose  from 'mongoose'
-/*const connectDb=async()=>{
+const connectDb=async()=>{
     return await mongoose.connect(process.env.DB).then(()=>{
-       console.log("database connection established");
+        console.log("database connection established");
        
     })
     .catch((err)=>{
         console.log(`error to connect database:${err}`);
     })
 }
-*/
-const connectDb = async () => {
-    console.log("MongoDB URI:", process.env.DB);  // Log the URI to check if it's being loaded
-    return await mongoose.connect(process.env.DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-      .then(() => {
-        console.log("Database connection established");
-      })
-      .catch((err) => {
-        console.log(`Error to connect database: ${err}`);
-      });
-  }
+
   export default connectDb;
