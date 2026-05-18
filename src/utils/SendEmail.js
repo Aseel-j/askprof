d9c2dbf253411fb8b1d04d320678226cde7efaaf
Aseel-j <aseelshaherjumaa2024@gmail.com>
2026-05-18 03:47:14 +0300

    Update SendEmail.js

--- src/utils/SendEmail.js
+++ src/utils/SendEmail.js
@@ -17,10 +17,15 @@ import nodemailer from "nodemailer";
 export async function sendEmail(to, subject, html) {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // مهم جدًا
       auth: {
         user: process.env.SENDER_EMAIL,
         pass: process.env.SENDER_EMAIL_PASS,
+      },
+      tls: {
+        rejectUnauthorized: false,
       },
     });
 
