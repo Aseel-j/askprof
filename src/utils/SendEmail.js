52673bc9b6d31c447895d0afa4eef6b81bcfee3d
Aseel-j <aseelshaherjumaa2024@gmail.com>
2026-05-18 04:44:20 +0300

    Update email sender address in SendEmail function

--- src/utils/SendEmail.js
+++ src/utils/SendEmail.js
@@ -21,7 +21,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail(to, subject, html) {
   try {
     const response = await resend.emails.send({
-      from: "Ask Professional <no-reply@yourapp.com>",
+      from: "Ask Professional <onboarding@resend.dev>",
       to,
       subject,
       html,
